4b0c7d6502b636c09f4967e0330c6fc7b425bb58
MightyBoolik <bkuchaev@gmail.com>
2021-03-22 12:25:29 +0500

    Fix SearchConfig

--- src/test/java/config/SearchConfig.java
+++ src/test/java/config/SearchConfig.java
@@ -4,7 +4,7 @@ import org.aeonbits.owner.Config;
 @Config.LoadPolicy(Config.LoadType.MERGE)
 @Config.Sources({
         "system:properties",
-        "classpath:config.properties"
+        "classpath:remote_config.properties"
 })
 public interface SearchConfig extends Config {
     @Config.Key("text")
